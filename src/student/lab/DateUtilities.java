b2b339b7f25549658b9f14883be01f36d673ae47
mattkirchner <mattkirchner@192.168.1.15>
2017-11-06 13:44:58 -0600

    created DateUtilities class in the student.lab package

---
+++ src/student/lab/DateUtilities.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package student.lab;
+
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+
+/**
+ *
+ * @author mattkirchner
+ */
+public class DateUtilities {
+    //with default pattern
+    public final String toStringDefaultFormat(LocalDateTime date) throws IllegalArgumentException {
+        if (date == null) {
+            throw new IllegalArgumentException("Error: date argument cannot be null");
+        }
+        DateTimeFormatter defaultFormat = DateTimeFormatter.ofPattern("MM dd yyyy");
+        return defaultFormat.format(date);
+    }
+    
+    //with custom format
+    public final String toStringCustomnFormat(LocalDateTime date, String custFormat) throws IllegalArgumentException {
+        if (date == null) {
+            throw new IllegalArgumentException("Error: date argument cannot be null");
+        }
+        DateTimeFormatter defaultFormat = DateTimeFormatter.ofPattern(custFormat);
+        return defaultFormat.format(date);
+    }
+    
+    //with default format
+    public final LocalDateTime toLocalDateTimeDefaultFormat(String date){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM dd yyyy");
+        return LocalDateTime.parse(date, formatter);
+    }
+    
+    //with custom format
+    public final LocalDateTime toLocalDateTimecustomFormat(String date, String custFormat){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(custFormat);
+        return LocalDateTime.parse(date, formatter);
+    }
+}
