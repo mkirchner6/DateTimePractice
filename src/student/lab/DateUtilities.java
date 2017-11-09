232cb798105ed8e4337aea55a94cc2fc1f7e36f6
mattkirchner <mattkirchner@192.168.1.15>
2017-11-08 23:31:19 -0600

    added javadoc to date utilities

--- src/student/lab/DateUtilities.java
+++ src/student/lab/DateUtilities.java
@@ -15,7 +15,13 @@ import java.time.format.DateTimeFormatter;
  * @author mattkirchner
  */
 public class DateUtilities {
-    //with default pattern
+    /**
+     * Format a LocalDateTime object into a string based on a default format
+     *
+     * @param LocalDateTime 
+     * @return a string based on the default format
+     * @throws IllegalArgumentException if pattern is not recognized
+     */
     public final String toStringDefaultFormat(LocalDateTime date) throws IllegalArgumentException {
         if (date == null) {
             throw new IllegalArgumentException("Error: date argument cannot be null");
@@ -24,7 +30,15 @@ public class DateUtilities {
         return defaultFormat.format(date);
     }
     
-    //with custom format
+    /**
+     * Format a LocalDateTime object into a string based on the specified pattern.
+     *
+     * @param LocalDateTime 
+     * @param pattern - a String to format the LocalDateTime
+     * @return a String based on the specified pattern
+     * @throws IllegalArgumentException if date is not recognized
+     * @throws ParseException if date string cannot be parsed as a date
+     */
     public final String toStringCustomnFormat(LocalDateTime date, String custFormat) throws IllegalArgumentException {
         if (date == null) {
             throw new IllegalArgumentException("Error: date argument cannot be null");
@@ -33,13 +47,27 @@ public class DateUtilities {
         return defaultFormat.format(date);
     }
     
-    //with default format
+    /**
+     * Format a String based on a default pattern.
+     *
+     * @param String
+     * @return a LocalDateTime based on a default pattern
+     * @throws IllegalArgumentException if date is not recognized
+     */
     public final LocalDateTime toLocalDateTimeDefaultFormat(String date) throws ParseException{
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM dd yyyy");
         return LocalDateTime.parse(date, formatter);
     }
     
-    //with custom format
+    /**
+     * Format a String based on a default pattern.
+     *
+     * @param String
+     * @param pattern - a String to format the LocalDateTime
+     * @return a LocalDateTime based on a default pattern
+     * @throws IllegalArgumentException if date is not recognized
+     * @throws ParseException if date string cannot be parsed as a date
+     */
     public final LocalDateTime toLocalDateTimecustomFormat(String date, String custFormat) throws ParseException{
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(custFormat);
         return LocalDateTime.parse(date, formatter);
